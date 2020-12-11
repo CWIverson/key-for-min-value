f968c93cf36466a3addc21ba11234cd0d51ab125
christian iverson <chris.w.iverson@gmail.com>
2020-12-11 15:52:32 -0800

    commit tree c9523159b3516422ecf55b111f836206dab42355

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,25 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+   if name_hash.empty? 
+    return nil 
+   end
+    name_array=name_hash.collect{
+        |name, num| name
+    }
 
+    num_array=name_hash.collect{
+        |name, num| num
+    }
+    a=0
+    b=1
+    c=2
+    result_array=[]
+    if num_array[a] < num_array[b]
+        name_array[a]
+    elsif num_array[c]< num_array[b]
+        name_array[c]
+    else
+        name_array[b]
+    end
 end
